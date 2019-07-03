f931bca1b08c686f23663d7a872a4e62106de8d7
sameekshaHCL <50136702+sameekshaHCL@users.noreply.github.com>
2019-07-03 16:23:28 +0530

    New Changes With Dynamo DB

--- TimeLineApi/config.php
+++ TimeLineApi/config.php
@@ -3,11 +3,11 @@
 * This file is for configuration settings 
 */
 
-//$config_cstm = array();
 $config_cstm['sugar_base_url'] = 'https://sugar-stage.lightspeedone.com/rest/v11';
-$config_cstm['sugar_userName'] = 'admin123';
+$config_cstm['sugar_userName'] = 'AgrawalSa';
 $config_cstm['sugar_clientID'] = 'sugar';
 $config_cstm['sugar_clientSecret'] = '';
-$config_cstm['sugar_hash'] = 'Password123';
+$config_cstm['sugar_hash'] = base64_decode('VGVzdDEyMzQ=');
 $config_cstm['sugar_grant'] = 'password';
 $config_cstm['platform'] = 'base';
+$config_cstm['table'] = 'SugarCRM_tokens';
