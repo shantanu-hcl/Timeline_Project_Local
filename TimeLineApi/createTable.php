21ad29cc417720144e3cad609c64e8c1a0835764
sameekshaHCL <50136702+sameekshaHCL@users.noreply.github.com>
2019-07-03 16:28:20 +0530

    New Changes with Dynamo DB

---
+++ TimeLineApi/createTable.php
@@ -0,0 +1,45 @@
+require 'awsSDK\aws-autoloader.php';
+
+date_default_timezone_set('UTC');
+
+use Aws\DynamoDb\Exception\DynamoDbException;
+
+$sdk = new Aws\Sdk([
+    'endpoint'   => 'http://localhost:8000',
+    'region'   => 'local',
+    'version'  => 'latest'
+]);
+
+$dynamodb = $sdk->createDynamoDb();
+
+$params = [
+    'TableName' => 'SugarCRM_tokens',
+    'KeySchema' => [
+        [
+            'AttributeName' => 'username',
+            'KeyType' => 'HASH'  //Partition key
+        ]
+        
+    ],
+    'AttributeDefinitions' => [
+        [
+            'AttributeName' => 'username',
+            'AttributeType' => 'S'
+        ]
+       
+    ],
+    'ProvisionedThroughput' => [
+        'ReadCapacityUnits' => 10,
+        'WriteCapacityUnits' => 10
+    ]
+];
+
+try {
+    $result = $dynamodb->createTable($params);
+    echo 'Created table.  Status: ' . 
+        $result['TableDescription']['TableStatus'] ."\n";
+
+} catch (DynamoDbException $e) {
+    echo "Unable to create table:\n";
+    echo $e->getMessage() . "\n";
+}
